d0430ccc84e9d08ce36819d46fd0eae6808955a4
erfajc97 <erfacrypto@gmail.com>
2023-07-13 10:51:06 -0300

    tailwind install

--- Main.jsx
+++ Main.jsx
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './Main';
 
-export default function App() {
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Main() {
   return (
     <View style={styles.container}>
-      <Main/>
-      <StatusBar style="auto" />
+      <Text>Soy Main</Text>
     </View>
   );
 }
